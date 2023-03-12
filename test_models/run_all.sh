d998ecc6f4f4bd5b582ac126781260d8af955469
gpiat <guilhem.piat@cea.fr>
2023-03-12 01:08:46 +0100

    added UMLS and bert-base models to run_all script changed run_all.slurm to require caller to give relative path -- makes more sense, and allows using huggingface model names changed bioqa to reasoning-free because reasoning-required doesn't work

--- test_models/run_all.sh
+++ test_models/run_all.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-for MODEL in 20230308_231002_PMC123_model 20230308_232115_HybridFull_model 20230308_232115_HybridHalf_model; do
+#for MODEL in 20230308_231002_PMC123_model 20230308_232115_HybridFull_model 20230308_232115_HybridHalf_model; do
+for MODEL in ../20230308_231002_PMC123_model ../20230308_232115_HybridFull_model ../20230308_232115_HybridHalf_model ../20230304_010627_UMLS_model_lr2e-6_mlm0.15_1ep bert-base-uncased; do
     NAME=`echo $MODEL | cut -d '_' -f 3`
     sbatch --error='all_'$NAME'.err' --output='all_'$NAME'.out' run_all.slurm $MODEL $NAME
 done
